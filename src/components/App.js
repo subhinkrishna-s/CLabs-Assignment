39b64d5060b960699281ad3b1be180a43c19682b
subhinkrishna-s <subhinkrishna.suresh@gmail.com>
2024-07-27 19:14:36 +0530

    testing get request on live

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import '../css/App.css';
 import '../css/main.css';
 import Popup from './Popup';
@@ -7,27 +7,50 @@ import Popup from './Popup';
 function App() {
 
   const [popupStatus, setPopupStatus] = useState(false)
+  const [webhookData, setWebhookData] = useState(null)
 
-  fetch('https://webhook.site/token/9a91f094-64f6-4960-9f4f-3c9d1a937d3b/requests', {
-    method: 'GET',
-    headers: {
-        Accept:'application/json',
-        'Content-Type':'application/json',
-    }
-  })
-  .then((resp) => resp.text())
-  .then((data) => {
-      alert('Data alert')
-      console.log('Received Data: ',data)
-  }).catch((error)=>{
-      alert('Err Occ')
-      console.log('Error in getting the items from the Server:',error)
-      if (error.response) {
-        console.log('Response:', error.response);
-        console.log('Status:', error.response.status);
-        console.log('Headers:', error.response.headers);
+  useEffect(()=>{
+    fetch('https://cors-anywhere.herokuapp.com/https://webhook.site/token/9a91f094-64f6-4960-9f4f-3c9d1a937d3b/requests', {
+      method: 'GET',
+      headers: {
+          Accept:'application/json',
+          'Content-Type':'application/json',
       }
-  })
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+        alert('Data alert')
+
+        console.log('Received Data: ',data)
+  
+        console.log('Received Data content: ',data.data)
+
+        // const receivedData = data.data
+
+        // if(receivedData.length>0){
+        //   if(receivedData.length>1){
+        //     const receivedDataArr = data.data
+        //     receivedDataArr
+        //   }else{
+        //     setWebhookData(data.data[0].content)
+        //   }
+        // }else{
+        //   console.log('No Data found from Web Hook')
+        //   setWebhookData({})
+        // }
+  
+    }).catch((error)=>{
+        alert('Err Occ')
+        console.log('Error in getting the items from the Server:',error)
+        if (error.response) {
+          console.log('Response:', error.response);
+          console.log('Status:', error.response.status);
+          console.log('Headers:', error.response.headers);
+        }
+    })
+  },[])
+
+  console.log('Data Webhook: ',webhookData)
 
   return (
     <div className="container-fluid">
